2b9799b860bd07fe72167f5cb2c19d7ef0afa6a7
Fin McCarthy <fmccarthy@motumsimulation.com>
2021-07-19 13:32:26 +1000

    Removed Spaced library

--- Antidote.JiraListener/AntidoteJiraListener.py
+++ Antidote.JiraListener/AntidoteJiraListener.py
@@ -1,13 +1,25 @@
 import os
 import flask
+from boto3 import session
 from flask import request
-from spaces import spaces
 
 #from util.utils import get_keyvault_secret
 
+ACCESS_ID = str("THJ2HKRSFAH6RTJ6W43O")
+SECRET_KEY = str("NWgzBh1kBJqgGyJL99AZ0tI8HjGryPRyw4CRm8OwLYY")
+REGION = str("sfo3")
+
+s3sesseion = session.Session()
+
+def getS3Resource() :
+    return  s3sesseion.resource('s3',
+                    region_name=REGION,
+                    endpoint_url='https://'+REGION+'.digitaloceanspaces.com',
+                    aws_access_key_id=ACCESS_ID,
+                    aws_secret_access_key=SECRET_KEY)
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
-
 
 #secrets/env
 userJira = "production@antidote.com.au"
@@ -48,7 +60,7 @@ def PersistRequstData(requestData, ticketNo):
 
     open(localFilename, "wb").write(requestData) #stream to file
 
-    result = spaces.upload_file(spaces_name, spaces_region, localFilename, uploadFilename)
+    result = upload_file(spaces_name, spaces_region, localFilename, uploadFilename)
 
     if result == "Success":
         os.remove(localFilename)
@@ -57,6 +69,20 @@ def createDirectories():
     #Create save directory 
     if(not os.path.isdir(jiraFileDirectory)):
         os.mkdir(jiraFileDirectory)
+
+def upload_file(space_name, local_file, upload_name):
+    s3  = getS3Resource()
+    try:
+        s3.upload_file(local_file, space_name, upload_name)
+        message = "Success"
+        return message
+        # pass
+    except Exception as e:
+        message = "Error occured uloading file " + upload_name + " " + str(e)
+
+    return message
+    #upload_file('my-space-name', 'sfo2', 'test.txt', 'me1.txt')
+
 
 # Lets Go!
 #app.run(host="0.0.0.0", port=8080)
