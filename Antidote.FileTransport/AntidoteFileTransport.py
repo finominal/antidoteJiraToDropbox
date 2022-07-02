ee8c3bca2597dc2335c16892f54527c47327b49b
fmccarthy@motumsimulation.com <fmccarthy@motumsimulation.com>
2022-07-02 12:03:14 +1000

    Add Filename

--- Antidote.FileTransport/AntidoteFileTransport.py
+++ Antidote.FileTransport/AntidoteFileTransport.py
@@ -54,8 +54,7 @@ class JiraAttachment:
     customfield_10120: str #used to custom set the folder name 
 
     def getDestinationFilename(self) -> str:
-        return "/" + self.customfield_10120.strip()
-        #return  "/" + self.issueType.strip() + "/" + self.ticketNumber + "_" + self.summary.strip() + "/" + self.filename.strip()
+        return "/" + self.customfield_10120.strip() / + "/" + self.ticketNumber + "_" + self.summary.strip() + "/" + self.filename.strip()
         
 
 def SendHeartBeat(url):
